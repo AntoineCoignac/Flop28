510aa78f8400eb61b4a1215499be78f63ef18c49
AntoineCoignac <acoignac35@gmail.com>
2023-12-08 00:10:55 +0100

    questions et fichiers

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,43 @@
-import logo from './logo.svg';
-import './App.scss';
+import logo from "./images/icons/logo.svg";
+import "./App.scss";
+import Slide from "./components/slide/Slide";
+import Score from "./components/score/Score";
+import Avatar from "./components/avatar/Avatar";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({
+    name : null,
+    avatar : null,
+    country : null,
+    flag : null,
+    score : 13.7
+  });
+
+  const questions = [
+    {
+      question : "",
+      responses : [
+        {
+          response : {
+            text : "",
+            score : 0
+          }
+        }
+      ]
+    },
+  ]
+
   return (
     <div className="App">
-      
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="infos">
+        <Score />
+        <Avatar />
+      </div>
+      <Slide />
     </div>
   );
 }
